c51d0be42a5587286c12983b64e99dbd3fa10af8
RobertTTaylor9517 <robt15@hotmail.com>
2020-01-08 11:39:48 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -4,12 +4,26 @@ document.addEventListener("DOMContentLoaded", () => {
 
   task.addEventListener("submit", function(event){
     event.preventDefault();
+    let i = 1;
 
     let listEntry = task.querySelector('#new-task-description').value;
+    let textColor = task.querySelector('#new-task-color').value;
     let li = document.createElement('li')
-    li.textContent = listEntry;
-    todoList.appendChild(li);
 
+    li.textContent = listEntry;
+    li.setAttribute("class", "list-entry")
+    li.style.color = textColor;
+    li.addEventListener('click', deleteEntry)
+    
+    todoList.appendChild(li)
     task.reset();
   })
+
+  function deleteEntry(event){
+    console.log(event);
+    let target = event.target
+    console.log(event.target)
+    target.remove();
+  }
+
 });
